0a129e0f2513446721ff8fd9b331c1801d6b6530
Tatyana Kachan <155998480+Tatata1965@users.noreply.github.com>
2024-01-10 22:30:18 +0200

    Add files via upload

---
+++ DZ_9.py
@@ -0,0 +1,16 @@
+
+
+try:
+    a =  input('введите числа')
+    print(int (a[0])/int(a[1]))
+except ZeroDivisionError as er:
+    print(er)
+    b = input('введите число повторно, вторая цифра должна быть не ноль')
+    while b[1]!=0:
+        print(int(b[0]) / int(b[1]))
+        break
+    else:
+        print('введите число повторно')
+
+except ValueError:
+    print('были введены буквы')
